89993399e36b0f271f84c2b988ea3a377438cc0f
Tony <Tony@Lenovo-PC>
2016-11-18 17:47:21 +0100

    Merge origin/master
    Conflicts:
    	README.md

--- src/models/Customer.java
+++ src/models/Customer.java
@@ -1,28 +1,20 @@
 package models;
 
-import java.util.UUID;
-
 /**
- *
  * @author SirhH
  * @author DBoy67
  */
 public class Customer {
     private String customerName;
-    private String customerID;
     
     public Customer(String customerName){
-    	customerID = UUID.randomUUID().toString();
         this.customerName = customerName;
     }
     
     public String getCustomerName(){
         return customerName;
     }
-    
-    public String getCustomerID(){
-    	return customerID;
-    }
+  
     @Override
     public String toString(){
         return "Customer: " + customerName;
