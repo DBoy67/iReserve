5a0fae0cb4d7fb8df1d7ca2345e84239c6e7f875
Sir H <Sir H@SirH-PC.lan>
2016-11-18 15:45:01 +0100

    added methods getNextFreeSeat, toggleSeatStatus and a toSting. Also changed name in selectSeat to putSeat

--- src/models/Airplane.java
+++ src/models/Airplane.java
@@ -2,6 +2,7 @@ package models;
 
 import utilities.TravelClass;
 import java.util.HashMap;
+import utilities.SeatStatus;
 
 /**
  * @author Surekha
@@ -81,7 +82,7 @@ public class Airplane {
 	 * @param seatNo
 	 * @param seat
 	 */
-	public void selectSeat(Integer seatNo , Seat seat){
+	public void putSeat(Integer seatNo , Seat seat){
 		seatMap.put(seatNo, seat);
 	}
 
@@ -92,6 +93,39 @@ public class Airplane {
 	public Seat getSeat(Integer seatNo){
 		return seatMap.get(seatNo);
 	}
+        /**
+         * 
+         * Takes a @param travelClass to look for free seats for that travelClass
+         * and @return the first found free seat, if no seat is found the return
+         * will be null
+         */
+        public Seat getNextFreeSeat(TravelClass travelClass){
+            for(Seat s: seatMap.values()){
+                if(s.getSeatStatus()==SeatStatus.FREE && s.getTravelClass()==travelClass){
+                    return s;
+                }
+            }
+            return null;
+        }
+        /**
+         * 
+         * @param seat
+         * Takes the specified seat and sets an occupied seat to free and vise
+         * versa by calling the methods in the seat class
+         */
+        public void toggleSeatStatus(Seat seat){
+            if(seat.getSeatStatus()==SeatStatus.OCCUPIED)
+		seat.setStatusFree();
+            else{
+                seat.setStatusOccupied();
+            }
+	}
+
+        
+        @Override
+        public String toString(){
+            return getPlaneName() + getSeats().toString();
+        }
 
 	/**
 	 * for test purpose
@@ -102,8 +136,8 @@ public class Airplane {
 		Airplane airplane = new Airplane(1710, "boeing777", 10);
 		Seat seat5 = new Seat(5, TravelClass.FIRST);
 		Seat seat2 = new Seat(2, TravelClass.ECONOMY);
-		airplane.selectSeat(5,seat5);
-		airplane.selectSeat(2, seat2);
+		airplane.putSeat(5,seat5);
+		airplane.putSeat(2, seat2);
 		System.out.println("is seat no 2 booked ? : " + airplane.seatMap.containsKey(2) );
 		System.out.println("is seat no 7 booked ? : " + airplane.seatMap.containsKey(7) );
 		System.out.println("Total seats booked in the plane : " +airplane.seatMap.size());
