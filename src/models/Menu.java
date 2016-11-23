85c46ffb4c77f128ee54f65eee39e731f033d68b
SirhH <SirhH@GCZ7J32>
2016-11-23 11:38:20 +0100

    Menu ready to use

--- src/models/Menu.java
+++ src/models/Menu.java
@@ -5,10 +5,87 @@
  */
 package models;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  *
  * @author Tony Eriksson <Tony.Eriksson>
  */
 public class Menu {
+    List<Food> menu;
     
+    public Menu(){
+        menu=new ArrayList<>();
+        menu.add(new Food("Beef with fries in wine sauce", 129.90,"First Class"));
+        menu.add(new Food("Finest Lobster menu", 139.90,"First Class"));
+        menu.add(new Food("Warm soup menu", 49.90,"First Class"));
+        menu.add(new Food("Beer", 59.90,"First Class"));
+        menu.add(new Food("Wine", 89.90,"First Class"));
+        menu.add(new Food("Juice", 34.90,"First Class"));
+        
+        menu.add(new Food("Burger", 49.90,"Economy Class"));
+        menu.add(new Food("Carbonara", 69.90,"Economy Class"));
+        menu.add(new Food("Double sandwich", 39.90,"Economy Class"));
+        menu.add(new Food("Hot dog", 29.90,"Economy Class"));
+        menu.add(new Food("Coffee", 29.00,"Economy Class"));
+        menu.add(new Food("Soda", 36.50,"Economy Class"));
+        menu.add(new Food("Water", 10.00,"Economy Class"));
+
+    }
+    
+    public Food getFromEconomyClassMenu(int index){
+        return this.getEconomyClassMenu().get(index-1);
+    }
+    public Food getFromFirstClassMenu(int index){
+        return this.getFirstClassMenu().get(index-1);
+    }
+    
+    public List<Food> getEconomyClassMenu(){
+        List<Food> basic = new ArrayList<>();
+        for(int i=6; i<menu.size(); i++){
+            basic.add(menu.get(i));
+        }
+        return Collections.unmodifiableList(basic);
+    }
+    
+    public List<Food> getFirstClassMenu(){
+        return Collections.unmodifiableList(menu);
+    }
+    
+    public String EconomyClassMenutoString(){
+        StringBuilder sb = new StringBuilder();
+        int i=1;
+        for(Food food : this.getEconomyClassMenu()){
+            sb.append(i);
+            sb.append(". ");
+            i++;
+            sb.append(food);
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+    public String FirstClassMenutoString(){
+        StringBuilder sb = new StringBuilder();
+        int i=1;
+        for(Food food : this.getFirstClassMenu()){
+            sb.append(i);
+            sb.append(". ");
+            i++;
+            sb.append(food);
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+    //Running example
+    /*public static void main(String [] args){
+        Menu m = new Menu();
+        System.out.println(m.getEconomyClassMenu());
+        System.out.println(m.getFirstClassMenu());
+        System.out.println(m.EconomyClassMenutoString());
+        System.out.println(m.FirstClassMenutoString());
+        System.out.println(m.getFromEconomyClassMenu(1));
+        System.out.println(m.getFromFirstClassMenu(1));
+    }*/
 }
